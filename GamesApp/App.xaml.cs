6cb87a239c05e3c6005895215e3e6d2e3162c426
Eduard Nica <eduardnica00@gmail.com>
2024-05-12 10:55:01 +0300

    Add project files.

---
+++ GamesApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GamesApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ GamesApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GamesApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
