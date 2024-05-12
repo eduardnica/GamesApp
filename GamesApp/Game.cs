0a8ee0ab96c3100411dbd90877af1f6fdfc2a7cb
Eduard Nica <eduardnica00@gmail.com>
2024-05-12 11:35:14 +0300

    Created the snippet for the first page and the game class

---
+++ GamesApp/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamesApp
+{
+    public class Game
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string Developer { get; set; }
+        public int ReleaseDate { get; set; }
+        public string Category { get; set; }
+        public double Rating { get; set; }
+
+    }
+}
--- GamesApp/MainPage.xaml.cs
+++ GamesApp/MainPage.xaml.cs
@@ -2,24 +2,51 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+
+        public IList<Game> Games { get; }
 
         public MainPage()
         {
             InitializeComponent();
+
+            Games = new List<Game>
+            {
+            new Game
+                {
+                    Name = "The Witcher 3: Wild Hunt",
+                    Description = "An action role-playing game developed and published by CD Projekt.",
+                    Price = 29.99,
+                    Developer = "CD Projekt Red",
+                    ReleaseDate = 2015,
+                    Category = "Action RPG",
+                    Rating = 9.3
+                },
+                new Game
+                {
+                    Name = "Red Dead Redemption 2",
+                    Description = "An action-adventure game developed and published by Rockstar Games.",
+                    Price = 39.99,
+                    Developer = "Rockstar Games",
+                    ReleaseDate = 2018,
+                    Category = "Open World",
+                    Rating = 9.5
+                },
+                new Game
+                {
+                    Name = "The Legend of Zelda: Breath of the Wild",
+                    Description = "An action-adventure game developed and published by Nintendo.",
+                    Price = 49.99,
+                    Developer = "Nintendo",
+                    ReleaseDate = 2017,
+                    Category = "Action Adventure",
+                    Rating = 9.7
+                }
+            };
+            BindingContext = this;
+
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
+       
     }
 
 }
