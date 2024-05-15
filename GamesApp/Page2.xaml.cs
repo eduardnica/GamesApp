41fbbed99b2020f616c07cb3ed69ab932bce6e85
Eduard Nica <eduardnica00@gmail.com>
2024-05-15 18:37:27 +0300

    barchart with dummy data

--- GamesApp/Page2.xaml.cs
+++ GamesApp/Page2.xaml.cs
@@ -1,9 +1,54 @@
+using Microcharts;
+using SkiaSharp;
+using SQLite;
+
 namespace GamesApp;
 
 public partial class Page2 : ContentPage
 {
-	public Page2()
-	{
-		InitializeComponent();
-	}
+
+    ChartEntry[] entries = new[]
+    {
+
+                 new ChartEntry(600)
+                {
+                    Label = "The Witcher 3: Wild Hunt",
+                    ValueLabel = "112",
+                    Color = SKColor.Parse("#2d3e54") 
+
+                },
+                new ChartEntry(128)
+                {
+                    Label = "Red Dead Redemption 2",
+                    ValueLabel = "600",
+                    Color = SKColor.Parse("#2a3e54")
+
+                },
+                new ChartEntry(38)
+                {
+                    Label = "The Legend of Zelda: Breath of the Wild",
+                    ValueLabel = "38",
+                    Color = SKColor.Parse("#2b3e54")
+
+                }
+
+    };
+
+
+
+
+    public Page2()
+    {
+        InitializeComponent();
+
+        chartView.Chart = new BarChart
+        {
+            Entries = entries
+        };
+
+    }
+
+
+
+
 }
